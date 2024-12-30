cf7fde9b5b9bb1f4d9fe732a7fc6650d48ece0a4
Sofia Du <sofia.du@tecnico.ulisboa.pt>
2024-12-30 18:41:18 +0000

    changes 2025

--- takeaway/resources/py/specialBig.py
+++ takeaway/resources/py/specialBig.py
@@ -11,8 +11,8 @@ BOX205 = "30.50€"
 # Define menu items
 
 menu_items = [
-    {"number": "N203","title": "Califórnia e Maki de Salmão, Gunkan de Salmão, Sashimi de Salmão, Atum e Robalo, Nigiri de Salmão, Atum, Robalo e Gambas, Rolo de Ovo com Gambas Panadas e Sushi com Gambas Panadas e Cebola Frita", "price": BOX203, "image": "resources/img/special/n203.png"},
-    {"number": "N205","title": "Dragão, Califórnia e Maki de Salmão, Gunkan de Salmão Defumado, Sashimi de Salmão e Atum, Nigiri de Salmão, Salmão Defumado e Atum, Sushi de Queijo com Salmão e Salmão Defumado", "price": BOX205, "image": "resources/img/special/n205.png"}
+    {"number": "N203","title": "Califórnia e Maki de Salmão, Gunkan de Salmão, Sashimi de Salmão, Atum e Robalo, Nigiri de Salmão, Atum, Robalo e Gambas, Rolo de Ovo com Gambas Panadas e Sushi com Gambas Panadas e Cebola Frita", "price": BOX203, "image": "resources/img/special/203.jpg"},
+    {"number": "N205","title": "Dragão, Califórnia e Maki de Salmão, Gunkan de Salmão Defumado, Sashimi de Salmão e Atum, Nigiri de Salmão, Salmão Defumado e Atum, Sushi de Queijo com Salmão e Salmão Defumado", "price": BOX205, "image": "resources/img/special/205.jpg"}
 
 ]
 
