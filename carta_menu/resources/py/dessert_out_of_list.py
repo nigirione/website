11fbdd818f59de149a427e3767c9b2100c323474
Sofia Du <sofia.du@tecnico.ulisboa.pt>
2025-11-02 14:40:40 +0000

    remove dessert

--- carta_menu/resources/py/dessert_out_of_list.py
+++ carta_menu/resources/py/dessert_out_of_list.py
@@ -17,7 +17,6 @@ menu_items = [
   {"number": "705","title": "Banana Frita","price": "2.30€" , "image": "resources/img/705.png"},
   {"number": "706","title": "Gelado Frito","price": "2.30€" ,"image": "resources/img/706.png"},
   {"number": "707","title": "Gelado na Chapa","price": "2.30€" ,"image": "resources/img/707.png"},
-  {"number": "708","title": "Lichia","price": "2.30€" ,"image": "resources/img/708.jpg"},
 ]
 
 # Write menu items to JSON file
