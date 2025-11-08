56339c1be6db3e693a15af9b19115da728caa5cc
Sofia Du <sofia.du@tecnico.ulisboa.pt>
2025-11-08 14:41:41 +0000

    added new dishes

--- carta_menu/resources/js/functions.js
+++ carta_menu/resources/js/functions.js
@@ -49,7 +49,7 @@ function fetchAndDisplayMenu(jsonFile, menuDivId, menuDivClass) {
               <h6>${item.title}</h6>
             `;
           }
-        }else if (menuDivId === 'varied' || menuDivId === 'spicy' || menuDivId === 'new' ) {
+        }else if (menuDivId === 'varied' || menuDivId === 'spicy' ) {
           if (item.number === "B5" ) {
             menuItem.innerHTML = `
               <img src="${item.image}" alt="${item.title}">
@@ -65,6 +65,30 @@ function fetchAndDisplayMenu(jsonFile, menuDivId, menuDivClass) {
               <h4>${item.number}</h4>
               <h6>${item.title}</h6>
               <p>${item.pieces}</p>
+            `;
+          }
+        }else if (menuDivId === 'new' ) {
+          if (item.obv) {
+            menuItem.innerHTML = `
+              <img src="${item.image}" alt="${item.title}">
+              <h4>${item.number}</h4>
+              <h6>${item.title}</h6>
+              <p>${item.pieces}</p>
+              <p><strong style="color: rgb(210, 55, 57); font-size: 12px;">${item.obv}</strong></p>
+            `;
+          }else if (item.pieces) {  // New condition for 'obv' field
+            menuItem.innerHTML = `
+              <img src="${item.image}" alt="${item.title}">
+              <h4>${item.number}</h4>
+              <h6>${item.title}</h6>
+              <p>${item.pieces}</p>
+            `;
+          }else {
+            // For other menu IDs
+            menuItem.innerHTML = `
+              <img src="${item.image}" alt="${item.title}">
+              <h4>${item.number}</h4>
+              <h6>${item.title}</h6>
             `;
           }
         }else if (menuDivId === 'fried' ) {
--- carta_menu/resources/py/new_dishes.py
+++ carta_menu/resources/py/new_dishes.py
@@ -14,8 +14,10 @@ if not os.path.exists(directory):
 # Define menu items
 
 menu_items = [
-  {"number": "92","title": "Carpaccio de Salmão","pieces": "5 Peças" ,"image": "resources/img/92.jpg"},
-  {"number": "93","title": "Carpaccio Maracujá de Salmão","pieces": "5 Peças" ,"image": "resources/img/93.jpg"},
+  {"number": "92","title": "Carpaccio de Salmão","pieces": "5 Peças" ,"image": "resources/img/92.jpg", "obv": "Limitado a 2 pratos por mesa"},
+  {"number": "93","title": "Carpaccio Maracujá de Salmão","pieces": "5 Peças" ,"image": "resources/img/93.jpg", "obv": "Limitado a 2 pratos por mesa"},
+  {"number": "271","title": "Salmão Burrata Carpaccio","pieces": "","image": "resources/img/271.jpg"},
+  {"number": "212","title": "Sashimi Salmão Picante","pieces": "5 Peças","image": "resources/img/212.jpg", "obv": "Limitado a 2 pratos por mesa"},
   {"number": "230","title": "Califórnia Gambas Panadas BBQ","pieces": "4 Peças" ,"image": "resources/img/230.jpg"},
   {"number": "233","title": "Califórnia Frango Panadas BBQ","pieces": "4 Peças" ,"image": "resources/img/233.jpg"},
   {"number": "N7","title": "Nigiri Tofu","pieces": "2 Peças" ,"image": "resources/img/N7.jpg"},
@@ -27,7 +29,12 @@ menu_items = [
   {"number": "83","title": "Califórnia + Salmão Defumado c/ Molho de Frutas","pieces": "2 Peças" ,"image": "resources/img/N83.jpg"},
   {"number": "84","title": "Califórnia de Manga c/ Molho de Frutas","pieces": "4 Peças" ,"image": "resources/img/N84.jpg"},
   {"number": "85","title": "Sashimi Salmão c/ Molho de Frutas", "pieces": "5 Peças" ,"image": "resources/img/N85.jpg"},
-  {"number": "86","title": "Gukan de Salmão Defumado c/ Molho","pieces": "2 Peças" ,"image": "resources/img/N86.jpg"},
+  {"number": "86","title": "Gunkan de Salmão Defumado c/ Molho","pieces": "2 Peças" ,"image": "resources/img/N86.jpg"},
+  {"number": "87","title": "Gunkan de Salmão c/ Masago Preto","pieces": "2 Peças" ,"image": "resources/img/87.png"},
+  {"number": "227","title": "Califórnia de Salmão c/ Masago Preto","pieces": "2 Peças" ,"image": "resources/img/227.jpg"},
+  {"number": "371","title": "Maki Frito c/ Morangos","pieces": "4 Peças" ,"image": "resources/img/371.jpg"},
+  {"number": "0","title": "Pão Japonês","pieces": "1 Peça" ,"image": "resources/img/0.jpg"},
+  {"number": "V8","title": "Salada Goma Wakami","pieces": "", "image": "resources/img/V8.jpg"},
 ]
 
 # Write menu items to JSON file
